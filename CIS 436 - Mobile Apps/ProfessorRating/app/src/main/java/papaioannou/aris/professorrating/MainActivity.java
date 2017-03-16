1f27fafcb46710b2d80a668466c2f5582a0529f5
Aristotelis Papaioannou <adpapaio@UMICH.EDU>
2017-03-16 15:32:52 -0400

    fragment loading

--- CIS 436 - Mobile Apps/ProfessorRating/app/src/main/java/papaioannou/aris/professorrating/MainActivity.java
+++ CIS 436 - Mobile Apps/ProfessorRating/app/src/main/java/papaioannou/aris/professorrating/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_prof_list);
 
         ListView profList = (ListView) findViewById(R.id.professor_list);
 
--- CIS 436 - Mobile Apps/ProfessorRating/app/src/main/java/papaioannou/aris/professorrating/ProfListFragment.java
+++ CIS 436 - Mobile Apps/ProfessorRating/app/src/main/java/papaioannou/aris/professorrating/ProfListFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +21,8 @@ public class ProfListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+       View view = inflater.inflate(R.layout.fragment_prof_list,container,false);
+        return view;
     }
 
 }

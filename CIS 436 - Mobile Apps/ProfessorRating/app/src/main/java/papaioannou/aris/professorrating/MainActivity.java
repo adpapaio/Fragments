fbe2b4baac9746045921eafb7cb2ecd7becab2c6
Aristotelis Papaioannou <adpapaio@UMICH.EDU>
2017-03-16 14:48:36 -0400

    changes

--- CIS 436 - Mobile Apps/ProfessorRating/app/src/main/java/papaioannou/aris/professorrating/MainActivity.java
+++ CIS 436 - Mobile Apps/ProfessorRating/app/src/main/java/papaioannou/aris/professorrating/MainActivity.java
@@ -1,5 +1,6 @@
 package papaioannou.aris.professorrating;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView profList = (ListView) findViewById(R.id.professor_list);
+
+        List<String> array_prof_list = new ArrayList<String>();
+        array_prof_list.add("John Baugh");
+        array_prof_list.add("David Yoon");
+        array_prof_list.add("Bruce Maxim");
+        array_prof_list.add("Bruce Elenbogen");
+        array_prof_list.add("Maroune Kessentini");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array_prof_list);
+
+        profList.setAdapter(arrayAdapter);
 
     }
 }

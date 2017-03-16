81ac7ce4eb1dda485f2a9803685542a62cee5dc4
adpapaio <adpapaio@umich.edu>
2017-03-16 11:27:23 -0400

    Files

---
+++ CIS 436 - Mobile Apps/ProfessorRating/app/src/main/java/papaioannou/aris/professorrating/ProfessorInfo.java
@@ -0,0 +1,26 @@
+package papaioannou.aris.professorrating;
+
+/**
+ * Created by Aris on 3/16/2017.
+ */
+
+public class ProfessorInfo {
+
+    public static final String[] ProfNames = {
+
+            "John Baugh",
+            "Kiumi Akinbehir",
+            "Bruce Elenbogen",
+            "Jinhua Guo",
+            "Maroune Kessentini"
+
+    };
+
+    public static  final String[] ProfINFO = {
+        "JohnBaugh",
+            "Kiumi",
+            "Bruce",
+            "Guo",
+            "Kessentini"
+    };
+}
---
+++ CIS 436 - Mobile Apps/ProfessorRating/app/src/main/java/papaioannou/aris/professorrating/Titles.java
@@ -0,0 +1,19 @@
+package papaioannou.aris.professorrating;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Aris
+ */
+
+public class Titles extends ListFragment{
+
+    boolean twoPanes;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
